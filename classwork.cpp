d8f27333b3d4d5900798353932f9468717a0f04c
KevinOn-line <kevinon092@gmail.com>
2024-12-11 21:05:22 +0000

    I leanred \n and std::endl to create sentences and orgainze such.

--- classwork.cpp
+++ classwork.cpp
@@ -4,6 +4,11 @@
 int main()
 {
   // write code here
-  
+  int age = 10; // SET age to 10
+  double price = 19.99;
+  bool isRaining = false;
+  std::cout << age << "\n" << price << "\n" <<isRaining << std::endl;
+  int grade_level = 10;
+  std::cout << "I am a "<< grade_level << "th grader." << std::endl;
   return 0;
 }
